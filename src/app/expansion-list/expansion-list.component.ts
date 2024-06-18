ecd2e6bc22a5843e176ca2c393c4a55b6124171b
Loïc Duhamel <loic.duhamel95@gmail.com>
2024-06-18 11:37:22 +0200

    fix: responsive (#5)
    Co-authored-by: Loic <lduhamel@ineat.fr>

--- src/app/expansion-list/expansion-list.component.ts
+++ src/app/expansion-list/expansion-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-expansion-list',
   templateUrl: './expansion-list.component.html',
-  styleUrls: ['./expansion-list.component.css']
+  styleUrls: ['./expansion-list.component.css'],
+  animations: [
+    trigger('crossfade', [
+      transition(':increment, :decrement', [
+        style({ opacity: 0 }),
+        animate('1s ease-in-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class ExpansionListComponent implements OnInit {
+  currentIndex = 0;
+  images = ['assets/image/astek.png', 'assets/image/ineat.png'];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex === 0) ? 1 : 0;
+    }, 2500);
   }
 
 }
